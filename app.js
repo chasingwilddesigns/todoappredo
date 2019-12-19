7c61526fe015101421716f31513232e0ea53b4ae
Hinton <blissfullah@gmail.com>
2019-12-18 17:18:09 -0700

    add mdl

--- app.js
+++ app.js
@@ -1,50 +1,40 @@
 function onReady(){
-  let toDos = [];
+  const addToDoForm = document.getElementById('addToDoForm');
+  const newToDoText = document.getElementById('newToDoText');
+  const toDoList = document.getElementById('toDoList');
 
-  const ADD_TODO_FORM = document.getElementById('addToDoForm');
-  const NEW_TODO_TEXT = document.getElementById('newToDoText');
-  const TODO_LIST = document.getElementById('toDoList');
-
-  ADD_TODO_FORM.addEventListener('submit', (event) => {
+  addToDoForm.addEventListener('submit', (event) => {
     event.preventDefault();
 
-    let title = NEW_TODO_TEXT.value;
+    let title = newToDoText.value;
 
-    toDos.push(title);
-    console.log(toDos);
+    let newLi = document.createElement('li');
 
-    let newLi = document.createElement('li'); // <li></li>
     let checkbox = document.createElement('input');
+
     checkbox.type = "checkbox";
 
-    let deleteBtn = document.createElement('button');
-    deleteBtn.textContent = "Delete";
+    newLi.textContext = title;
 
-    deleteBtn.addEventListener('click', function(event){
-      let buttonLiText = this.parentElement.childNodes[0].textContent;
-      // console.log(event);
-      // this.parentElement represents the button's <li> parent
-      TODO_LIST.removeChild(this.parentElement);
-
-      toDos.forEach(function(currentToDo, index){
-        // console.log(currentToDo, index);
-        // console.log(this);
-
-        if(currentToDo === buttonLiText){
-          // remove from array
-          toDos.splice(index, 1);
-        }
-        console.log(toDos);
-      });
-    })
-
-    newLi.textContent = title;
     newLi.appendChild(checkbox);
-    newLi.appendChild(deleteBtn);
-    TODO_LIST.appendChild(newLi);
-    NEW_TODO_TEXT.value = "";
+
+    toDoList.appendChild(newLi);
+
+    newToDoText.value = '';
+
   });
-};
+
+}
+
+var checkbox = document.querySelector("input[name=checkbox]");
+
+checkbox.addEventListener( 'change', function() {
+    if(this.checked) {
+        // Checkbox is checked..
+    } else {
+        // Checkbox is not checked..
+    }
+});
 
 window.onload = function() {
   onReady();
